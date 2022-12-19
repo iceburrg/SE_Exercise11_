aa4117453932c46bec9454b0378bf30873ad4901
iceburrg <108740953+iceburrg@users.noreply.github.com>
2022-12-19 18:56:41 +0100

    Create Helloworld.java
    This commit will create the Helloworld.java in order to have something to fetch/pull.

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+/*
+ *this class is only used to have changes to fetch and pull 
+ */
+public class Helloworld {
+
+  public static void main (String args[]){
+    System.out.println("Hello world");
+  }
+}
